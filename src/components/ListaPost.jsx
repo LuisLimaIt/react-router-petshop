9615f2827c30f9a87b21de2a03f94d59843a7851
luislimait <pls.luis@gmail.com>
2022-01-27 12:55:25 -0300

    Exibindo Post

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { busca } from '../api/api';
 
-export default function ListaPost() {
-    return(
-        <section className="posts container">
+export default function ListaPost({ url }) {
+    const [posts, setPosts] = useState([]);
 
+    useEffect(() => {
+        busca(url, setPosts)
+    },[url]);
+
+    return(
+        <section className='posts container'>
+            {posts.map((post) => (
+                <Link 
+                    className={`cartao-post cartao-post--${post.categoria}`}
+                    to={`/posts/${post.id}`}
+                >
+                    <article key={post.id}>
+                        <h3 className='cartao-post__titulo'>
+                            {posts.title}
+                        </h3>
+                        <p className='cartao-post__meta'>{post.metadescription}</p>
+                    </article>
+                </Link>
+            ))}
         </section>
     )
 }
