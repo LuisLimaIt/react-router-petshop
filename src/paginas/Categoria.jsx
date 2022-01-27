fd612bbb0a5a86b1c4a95700840bbad3933565d3
luislimait <pls.luis@gmail.com>
2022-01-27 15:38:29 -0300

    Criando SubCategoria

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import { Route, useParams } from 'react-router-dom'
+import { busca } from '../api/api'
 import '../assets/css/blog.css'
 import { ListaCategorias } from '../components/ListaCategorias'
 import ListaPost from '../components/ListaPost'
@@ -9,16 +10,23 @@ export default function Categoria() {
 
     const { id } = useParams()
     const { path } = useRouteMatch()
+    const [subcategorias, setSubcategorias] = useState([])
 
+    useEffect(() => {
+        busca(`/categorias/${id}`, (categoria) => {
+            setSubcategorias(categoria.subcategorias)
+        })
+    }, [id]);
+    
     return(
         <>
-        <div className="container">
-            <h2 className="titulo-pagina">Pet Notícias</h2>
-        </div>
-        <ListaCategorias />
-        <Route exact path={`${path}/`}>
-            <ListaPost url={`/posts?categoria=${id}`} />
-        </Route>
+            <div className="container">
+                <h2 className="titulo-pagina">Pet Notícias</h2>
+            </div>
+            <ListaCategorias />
+            <Route exact path={`${path}/`}>
+                <ListaPost url={`/posts?categoria=${id}`} />
+            </Route>
         </>
     )
 }
