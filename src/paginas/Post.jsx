6e8a27f0f72d6e57b9d9c4d3b894dc4674800e2e
luislimait <pls.luis@gmail.com>
2022-01-27 13:02:52 -0300

    Post inexistente

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { busca } from '../api/api';
+import React, { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { busca } from '../api/api'
 import '../assets/css/post.css'
 
 export default function Post() {
-    const { id } = useParams();
-    const [post, setPost] = useState({});
+    let history = useHistory()
+    const { id } = useParams()
+    const [post, setPost] = useState({})
 
     useEffect(() => {
         busca(`/posts/${id}`, setPost)
+        .catch(() => {
+            history.push('/404')
+        })
     }, [id]);
 
     return(
